422655fe2886f28aafe59acfbee6d9d25bc9311f
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2022-11-17 23:33:30 -0300

    day12b is not working.

---
+++ kotlin-test/src/main/kotlin/advent2020/Day12b.kt
@@ -0,0 +1,65 @@
+package advent2020
+
+import kotlin.math.abs
+
+object Day12b : Solver {
+
+    val north = Pair(1, 0)
+    val east = Pair(0, 1)
+    val south = Pair(-1, 0)
+    val west = Pair(0, -1)
+
+    val directions = listOf(north, east, south, west)
+
+    data class State(val x: Int, val y: Int, val waypoint: Pair<Int, Int> = Pair(1, 10)) {
+        fun north(amount: Int) = State(x, y, waypoint.copy(first = waypoint.first + amount))
+        fun south(amount: Int) = State(x, y, waypoint.copy(first = waypoint.first - amount))
+        fun east(amount: Int) = State(x, y, waypoint.copy(first = waypoint.second + amount))
+        fun west(amount: Int) = State(x, y, waypoint.copy(first = waypoint.second - amount))
+        fun fwd(amount: Int) = State(x + waypoint.first * amount, y + waypoint.second * amount, waypoint)
+        fun rotateR() = this.copy(waypoint = Pair(-waypoint.second, waypoint.first))
+        fun rotateL() = this.copy(waypoint = Pair(waypoint.second, -waypoint.first))
+    }
+
+    data class Command(val ins: Char, val amount: Int) {
+        fun apply(state: State): State {
+            println(state)
+            return when (ins) {
+                'N' -> state.north(amount)
+                'S' -> state.south(amount)
+                'E' -> state.east(amount)
+                'W' -> state.west(amount)
+                'F' -> state.fwd(amount)
+                'R' -> (0 until amount/90).fold(state) {s, _ -> s.rotateR()}
+                'L' -> (0 until amount/90).fold(state) {s, _ -> s.rotateL()}
+                else -> TODO()
+            }
+        }
+    }
+
+    fun parse(line: String): Command = Command(line[0], line.substring(1).toInt())
+
+    override fun solve(input: List<String>): Any {
+        val end = input.map { parse(it) }.fold(State(0, 0)) { state, ins -> ins.apply(state) }
+        println(end)
+        return abs(end.x) + abs(end.y)
+    }
+
+}
+
+fun main() {
+    println(
+        Day12b.solve(
+            """
+        F10
+        N3
+        F7
+        R90
+        F11
+    """.trimIndent()
+        )
+    )
+
+//    println(Day12b.solve("day12.txt"))
+
+}
