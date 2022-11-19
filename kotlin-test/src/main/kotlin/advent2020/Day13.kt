3985f998b8482f610165116407ff7175fcbf48d1
Takeshi Fukushima <takeshi.fukushima@cittati.com.br>
2022-11-19 10:29:18 -0300

    day13b completed

---
+++ kotlin-test/src/main/kotlin/advent2020/Day13.kt
@@ -0,0 +1,85 @@
+package advent2020
+
+import java.math.BigInteger
+
+object Day13 : Solver {
+
+    fun minTime(time: Int, busId: Int): Int {
+        val div = time / busId
+        return ((div + 1) * busId) - time
+    }
+
+    override fun solve(input: List<String>): Any {
+        val time = input[0].toInt()
+        val departures = input[1].split(",").filter { it != "x" }.map { it.toInt() }
+        val selected = departures.map { Pair(it, minTime(time, it)) }.minBy { it.second }
+        return selected.first * selected.second
+    }
+
+
+    fun valid(time: Long, times: Iterable<IndexedValue<Int>>): Boolean {
+        return times.all { ((time + it.index) % it.value) == 0L }
+    }
+
+    data class Entry(val initial: BigInteger, val factor: BigInteger)
+
+    fun mod(v: BigInteger, m: BigInteger): BigInteger {
+        if(v < BigInteger.ZERO) {
+            return mod(v + m, m)
+        } else if(v >= m) {
+            return mod(v - m, m)
+        } else return v
+    }
+
+    fun reduce(one: Entry, two: Entry): Entry {
+        if (one.initial < BigInteger.ZERO || two.initial < BigInteger.ZERO) {
+            throw RuntimeException()
+        }
+        var r = one.initial
+        while ((r % two.factor) != two.initial) {
+            r += one.factor
+        }
+        return Entry(r, one.factor * two.factor)
+    }
+
+    fun reduce2(one: Entry, two: Entry): Entry {
+        var r = one.initial
+        while ((r % two.factor) != two.initial) {
+            r += one.factor
+        }
+        return Entry(r, one.factor * two.factor)
+    }
+
+    override fun solveb(input: List<String>): Any {
+        val departures = input[1].split(",")
+            .asSequence()
+            .map { if (it == "x") 0 else it.toInt() }
+            .withIndex()
+            .filter { it.value != 0 }
+            .sortedBy { it.value }
+            .map {
+                Entry(
+                    mod(BigInteger.valueOf((it.value.toLong() - it.index)), BigInteger.valueOf(it.value.toLong())),
+                    BigInteger.valueOf(it.value.toLong())
+                )
+            }
+            .toList()
+        val r = departures.drop(1).fold(departures.first()) { a, b -> reduce(a, b) }
+        return r.initial
+    }
+
+
+}
+
+fun main() {
+//    println(Day13.solveb(listOf("939", "7,13,x,x,59,x,31,19" )))
+    println(Day13.solveb(listOf("939", "67,7,59,61")))
+    println(
+        Day13.solveb(
+            """
+        1000655
+        17,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,571,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,x,x,x,x,23,x,x,x,x,x,29,x,401,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,19
+    """.trimIndent()
+        )
+    )
+}
--- kotlin-test/src/main/kotlin/advent2020/Solver.kt
+++ kotlin-test/src/main/kotlin/advent2020/Solver.kt
@@ -6,6 +6,23 @@ import java.io.FileReader
 
 interface Solver {
 
+    fun solveb(input: String): Any {
+        if (File(input).exists()) {
+            val reader = BufferedReader(FileReader(input))
+            return solveb(reader.lines().map { it.trim() }.toList())
+        } else if (File("2020", input).exists()) {
+            val reader = BufferedReader(FileReader(File("2020", input)))
+            return solveb(reader.lines().map { it.trim() }.toList())
+        } else if (File("kotlin-test/2020", input).exists()) {
+            val reader = BufferedReader(FileReader(File("kotlin-test/2020", input)))
+            return solveb(reader.lines().map { it.trim() }.toList())
+        }
+        return solveb(input.split("\n").toList())
+    }
+
+    fun solveb(input: List<String>): Any = solveI(input.map { Integer.parseInt(it) })
+    fun solveIb(input: List<Int>): Any = TODO()
+
     fun solve(input: String): Any {
         if (File(input).exists()) {
             val reader = BufferedReader(FileReader(input))
@@ -22,5 +39,4 @@ interface Solver {
 
     fun solve(input: List<String>): Any = solveI(input.map { Integer.parseInt(it) })
     fun solveI(input: List<Int>): Any = TODO()
-
 }
